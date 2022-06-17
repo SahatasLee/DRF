467f328514517d8d615f23d3e83bd606e0ca42b1
SahatasLee <Sahatasnutlee@gmail.com>
2022-06-17 14:08:58 +0700

    add authentication Register

--- api/views.py
+++ api/views.py
@@ -3,6 +3,14 @@ from .serializers import TaskSerializer
 from .models import Task
 from rest_framework import generics
 
+# Authentication
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer,RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework.authentication import TokenAuthentication
+
 
 class TaskList(generics.ListAPIView):
     queryset = Task.objects.all()
@@ -15,3 +23,17 @@ class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
 class TaskDetails(generics.RetrieveAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+# Class based view to Get User Details using Token Authentication
+class UserDetailAPI(APIView):
+  authentication_classes = (TokenAuthentication,)
+  permission_classes = (AllowAny,)
+  def get(self,request,*args,**kwargs):
+    user = User.objects.get(id=request.user.id)
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
+
+#Class based view to register user
+class RegisterUserAPIView(generics.CreateAPIView):
+  permission_classes = (AllowAny,)
+  serializer_class = RegisterSerializer
--- backend/urls.py
+++ backend/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('api/tasks/', views.TaskList.as_view()),
     path('api/tasks/<int:pk>', views.TaskDetail.as_view()),
     path('api/task/<int:pk>', views.TaskDetails.as_view()),
+    path("api/user/get-details", views.UserDetailAPI.as_view()),
+    path('api/user/register', views.RegisterUserAPIView.as_view()),
+    # path('api-token-auth', views.obtain_auth_token),
 ]
